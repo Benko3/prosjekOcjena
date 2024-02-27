cf52b299ecbc5e90e76b1dfb098dbd00b4557b34
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-02-27 12:15:49 +0100

    Add files via upload

---
+++ ProsjekOcjena.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ProsjekOcjena
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.unosOcjene = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // unosOcjene
+            // 
+            this.unosOcjene.Location = new System.Drawing.Point(157, 101);
+            this.unosOcjene.Name = "unosOcjene";
+            this.unosOcjene.Size = new System.Drawing.Size(100, 26);
+            this.unosOcjene.TabIndex = 0;
+            this.unosOcjene.TextChanged += new System.EventHandler(this.unosOcjene_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Unos ocjene:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Unesi ocjenu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 269);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prosjek ocjena";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unosOcjene);
+            this.Name = "form1";
+            this.Text = "Prosjek Ocjena";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox unosOcjene;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProsjekOcjena.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProsjekOcjena
+{
+    public partial class form1 : Form
+    {
+        int ocjena, brojOcjena = 0, zbroj;
+        double prosjek;
+
+        private void unosOcjene_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ocjena = Convert.ToInt32(unosOcjene.Text);
+            brojOcjena++;
+            zbroj += ocjena;
+            DialogResult odgovor = MessageBox.Show("Želiš li upisati još ocjena?", "Upit",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            switch (odgovor){
+                case DialogResult.Yes:
+                    textBox1.Clear();
+                    textBox1.Focus();
+                    break;
+                case DialogResult.No:
+                    prosjek = zbroj / brojOcjena;
+                    textBox1.Text = prosjek.ToString();
+                    textBox1.Text = ProsjekOcjena();
+                    break;
+
+            }
+        }
+    }
+}
